19fba9f99eaf798d14a52643a52c158a6271bd4a
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-21 11:38:57 +0900

    Feat: getAllPostIds, getPostData functions

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -8,7 +10,7 @@ export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
 
-  const allPostsData = fileNames.map(fileName => {
+  const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, "");
 
@@ -34,4 +36,32 @@ export function getSortedPostsData() {
       return -1;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: { id: fileName.replace(/\.md$/, "") },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content)
+    .toString();
+
+  return {
+    id,
+    processedContent,
+    ...(matterResult.data as { date: string; title: string }),
+  };
 }
