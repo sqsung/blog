c79f220801547668a0f9e0e7ec2aa655694c5291
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-24 21:41:01 +0900

    Fix: remove irrelevant comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { getSortedPostsData } from "../../lib/posts";
-import Header from "@/components/common/Header";
-
-/**
- * ✏️ NOTES:
- * 1. @getStaticProps
- * - When building highly performant static pages, but most likely without real-time data
- * - When page should be pre-rendered (SEO benefits) and must be fast
- *
- * 2. @getStaticPaths
- * - complements 'getStaticProps'
- * - When page has Dynamic Routes (must define a list of paths to be statistically generated)
- * - As the page builds, Next.js will statically pre-render all the paths specified by getStaticPaths
- * - 🌐 https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-paths
- *
- * 3. @getServerSideProps
- * - When you need Next.js to pre-render the page on each request (using the data returned from getServerSideProps)
- * - Basically, the better alternative to getStaticProps when you need to make a page with real-time data (frequent updates)
- */
 
 export default async function Home() {
   /**
@@ -29,12 +11,8 @@ export default async function Home() {
   const allPostsData = await getSortedPostsData();
 
   return (
-    <div className="w-sreen flex h-screen flex-col">
-      <head>
-        <title>James Next.js Blog</title>
-      </head>
-      <Header />
-      <section className="h-100 flex h-1/4 items-center justify-center gap-10 border p-10">
+    <div className="flex h-full w-full flex-col">
+      <section className="flex items-center justify-center gap-10 border p-5">
         <Image
           src="/images/profile.jpg"
           alt="Blog Author's profile picture"
@@ -51,7 +29,7 @@ export default async function Home() {
           </p>
         </div>
       </section>
-      <section className="h-100 h-3/4 flex-col items-center justify-center border p-10">
+      <section className="h-100 h-3/4 flex-col items-center justify-center p-10">
         <h2 className="my-5 text-2xl font-bold">Blogs</h2>
         <ul className="flex flex-col gap-10">
           {allPostsData.map(({ id, title, date }) => (
