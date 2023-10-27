3fa67d37ae65f8b4eb5e43ce6e3c5134937e1bd7
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-28 08:24:15 +0900

    Fix: removed /devlog route and made it the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { useEffect } from "react";
-import { AuthorProfile } from "@/components/common";
-import BlogList from "@/components/techBlogs/BlogList";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
+import BlogList from "@/components/devlog/BlogList";
 
-export default function Home() {
-  return (
-    <div className="flex w-5/6 justify-center gap-5 border">
-      <BlogList />
-    </div>
-  );
+export default function HomeDevlogPage() {
+  return <BlogList />;
 }
