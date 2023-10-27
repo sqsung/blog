d3ff794793c06fdfe04a0af2e64d0e985dee2f86
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-28 08:19:35 +0900

    Feat: formatDate util function

---
+++ src/utils/format.ts
@@ -0,0 +1,8 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const yyyy = date.getFullYear();
+  const mm = (date.getMonth() + 1).toString().padStart(2, "0");
+  const dd = date.getDate().toString().padStart(2, "0");
+
+  return `${yyyy}/${mm}/${dd}`;
+};
