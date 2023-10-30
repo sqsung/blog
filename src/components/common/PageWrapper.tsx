cfe0a0671cc9ca34f1d69b043b718a56d2e39da0
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-30 19:41:22 +0900

    Style: change page wrapper padding value

--- src/components/common/PageWrapper.tsx
+++ src/components/common/PageWrapper.tsx
@@ -4,7 +4,7 @@ interface PageWrapperProps {
 
 export default function PageWrapper({ children }: PageWrapperProps) {
   return (
-    <div className="border-test flex h-full min-h-screen w-full justify-center gap-5 px-40 py-10">
+    <div className="border-test flex h-full min-h-screen w-full justify-center gap-5 px-[15%] py-10">
       {children}
     </div>
   );
