aa3b5313c8177602282429bbe23c2e3d4ac8cea7
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-31 13:43:40 +0900

    Feat: Header/Footer components

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+import { ContactLinks } from ".";
+
+export default function Footer() {
+  return (
+    <div className="mt-auto flex h-[50px] items-center justify-between border-t-[1px] px-10">
+      <p className="italic text-gray-300">
+        © 2023 sqsung devlog. All Rights Reserved.
+      </p>
+      <ContactLinks />
+    </div>
+  );
+}
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="flex h-[50px] items-center border-b-[1px] ps-10">
+    <div className="mb-auto flex h-[50px] items-center border-b-[1px] ps-10">
       <Link href="/">
         <p className="cursor-pointer text-2xl font-bold italic text-blue-300">
           <span className="not-italic">🐋</span> sqsung devlog
