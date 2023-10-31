e19d30962fe0abd5ea0bb5b49e7ad965b1186953
Kyusung Sohn <rok.ksohn@gmail.com>
2023-10-31 13:44:31 +0900

    Fix: make entire BlogListItem a link to post page

--- src/components/devlog/BlogListItem.tsx
+++ src/components/devlog/BlogListItem.tsx
@@ -17,29 +17,28 @@ export default function BlogListItem(props: BlogListItemProps) {
 
   return (
     <li className="py-5 transition hover:scale-[1.01]">
-      <div className="flex h-[150px] gap-5">
-        {thumbnail && (
-          <Image
-            src={thumbnail}
-            width={150}
-            height={150}
-            alt="Blog Post Thumbnail"
-          />
-        )}
-        <div className="flex h-full w-full flex-col">
-          <Link
-            href={`/${id}`}
-            className="text-[20px] font-bold transition hover:cursor-pointer hover:text-gray-300"
-          >
-            {title}
-          </Link>
-          <div className="flex items-center gap-3">
-            <p className="text-sm text-gray-400">{formatDate(date)}</p>
-            <Tags tags={tags} />
+      <Link href={`/${id}`}>
+        <div className="flex h-[150px] gap-5">
+          {thumbnail && (
+            <Image
+              src={thumbnail}
+              width={150}
+              height={150}
+              alt="Blog Post Thumbnail"
+            />
+          )}
+          <div className="flex h-full w-full flex-col">
+            <p className="text-[20px] font-bold transition hover:cursor-pointer hover:text-gray-300">
+              {title}
+            </p>
+            <div className="flex items-center gap-3">
+              <p className="text-sm text-gray-400">{formatDate(date)}</p>
+              <Tags tags={tags} />
+            </div>
+            <p className="my-5">{description}</p>
           </div>
-          <p className="my-5">{description}</p>
         </div>
-      </div>
+      </Link>
     </li>
   );
 }
