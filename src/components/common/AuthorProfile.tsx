c3e0f55007c0f2733d8e79abd2afb867d7b16528
Kyusung Sohn <rok.ksohn@gmail.com>
2023-11-29 16:57:34 +0900

    Style: fix AuthorProfile responsive styles

--- src/components/common/AuthorProfile.tsx
+++ src/components/common/AuthorProfile.tsx
@@ -15,18 +15,18 @@ export default async function AuthorProfile() {
         className="h-[300px] w-full object-cover sm:w-[300px] sm:rounded-full"
       />
       <div className="flex w-full flex-col gap-3 p-5 sm:w-[540px]">
-        <div className="flex gap-2 sm:flex-col sm:gap-0">
+        <div className="flex gap-2 sm:flex-col sm:items-center sm:gap-0">
           <p className="text-lg font-bold md:text-2xl">James K. Sohn</p>
-          <p className="mr-auto flex items-center text-sm text-gray-400 sm:mb-3 sm:text-lg">
+          <p className="mr-auto flex items-center text-sm text-gray-400 sm:mb-3 sm:mr-0 sm:text-lg">
             Frontend Engineer
           </p>
           <ContactLinks color="text-gray-400" />
         </div>
-        <p className="sm:text-md text-sm text-gray-700">
+        <p className="sm:text-md text-sm text-gray-700 sm:text-center">
           A coffee loving frontend engineer from Korea with a passion for
           developing user centered products with innovative business models.
         </p>
-        <p className="sm:text-md text-sm text-gray-700">
+        <p className="sm:text-md text-sm text-gray-700 sm:text-center">
           {`Accidentally fell in love with dev while studying it to manage my
           startup${"'"}s development team. Been a full-time dev since.`}
         </p>
