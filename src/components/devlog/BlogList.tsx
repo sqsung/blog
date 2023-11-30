140f018c8c8dc337c9d476187ea3b7df6a06f131
Kyusung Sohn <rok.ksohn@gmail.com>
2023-11-30 10:48:28 +0900

    Fix: BlogListItem reponsive style complete

--- src/components/devlog/BlogList.tsx
+++ src/components/devlog/BlogList.tsx
@@ -9,7 +9,7 @@ export default async function BlogList() {
       {/* <div>
         <p className="title w-full text-center">Recent Posts:</p>
       </div> */}
-      <div className="mt-5 w-full justify-center px-0 sm:flex sm:px-[15%]">
+      <div className="mt-5 w-full justify-center px-5 sm:flex sm:px-[10%] md:px-[15%]">
         <ul className="flex w-full flex-col justify-center gap-5">
           {allPostsData.map((metaData, index) => (
             <BlogListItem key={index} {...metaData} />
--- src/components/devlog/BlogListItem.tsx
+++ src/components/devlog/BlogListItem.tsx
@@ -9,26 +9,25 @@ export default function BlogListItem(props: PostData) {
   const { id, thumbnail, tags, title, description, date, category } = props;
 
   return (
-    <li className="sm:border-gray-150 h-30 overflow-hidden border-b pb-5 transition hover:border-gray-700 sm:rounded-md sm:border sm:pb-0 [&:first-of-type]:border-t">
+    <li className="sm:border-gray-150 h-80 overflow-hidden rounded-md border transition hover:border-gray-700 sm:h-fit sm:pb-0">
       <Link href={`/categories/${category}/${id}`}>
-        <div className="flex h-[100%] gap-2 sm:gap-5">
+        <div className="flex h-[100%] flex-col gap-2 sm:flex-row">
           <Image
             src={thumbnail || "/seriesThumbnails/nextjs.png"}
             width={100}
             height={100}
             alt="Blog Post Thumbnail"
-            objectFit="cover"
-            className="max-h-[100px] sm:h-[200px] sm:max-h-[200px] sm:w-[200px]"
+            className="h-[60%] w-full object-cover sm:h-[200px] sm:max-h-[200px] sm:w-[200px]"
           />
-          <div className="flex h-full w-full flex-col sm:py-3">
-            <p className="text-sm font-bold transition hover:cursor-pointer hover:text-gray-300 sm:text-lg">
+          <div className="flex h-full w-full flex-col p-1 sm:p-2">
+            <p className="overflow-ellipsis whitespace-nowrap text-sm font-bold transition hover:cursor-pointer hover:text-gray-300 sm:text-lg">
               {title}
             </p>
             <p className="text-xs text-gray-400 sm:text-sm ">
               {`${formatDate(date)}`}
             </p>
             <Tags tags={tags} />
-            <p className="my-1 h-full overflow-hidden py-1 text-xs text-gray-500 sm:py-5 sm:text-base">
+            <p className="my-1 line-clamp-2 overflow-hidden overflow-ellipsis py-1 text-xs text-gray-500 sm:my-3 sm:text-base">
               {description}
             </p>
           </div>
