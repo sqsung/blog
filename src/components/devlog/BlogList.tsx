ea8ed443d95a7caff9b4fb9b2ab181edc7591e7b
Kyusung Sohn <rok.ksohn@gmail.com>
2023-11-30 13:19:02 +0900

    Feat: see all categories button

--- src/components/devlog/BlogList.tsx
+++ src/components/devlog/BlogList.tsx
@@ -1,16 +1,24 @@
 import BlogListItem from "./BlogListItem";
 import { getLatestTenPostsData } from "../../../lib/posts";
+import Link from "next/link";
 
 export default async function BlogList() {
   const latestTenPosts = await getLatestTenPostsData();
 
   return (
-    <div className="mt-5 w-full justify-center px-5 sm:px-[10%] md:px-[15%]">
+    <div className="mt-5 flex w-full flex-col justify-center gap-5 px-5 pb-10 sm:px-[10%] md:px-[15%]">
       <ul className="flex w-full flex-col justify-center gap-5">
         {latestTenPosts.map((metaData, index) => (
           <BlogListItem key={index} {...metaData} />
         ))}
       </ul>
+      <Link
+        href="/categories"
+        className="flex justify-center gap-1 text-gray-500 transition hover:text-gray-700"
+      >
+        <span>See All Categories</span>
+        <i className="bi bi-arrow-right" />
+      </Link>
     </div>
   );
 }
