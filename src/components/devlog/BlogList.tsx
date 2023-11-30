0023910b548334034b0ba67cb252c5a8419e0dc4
Kyusung Sohn <rok.ksohn@gmail.com>
2023-11-30 13:08:46 +0900

    Fix: remove unnecessary code from BlogList

--- src/components/devlog/BlogList.tsx
+++ src/components/devlog/BlogList.tsx
@@ -5,14 +5,12 @@ export default async function BlogList() {
   const latestTenPosts = await getLatestTenPostsData();
 
   return (
-    <>
-      <div className="mt-5 w-full justify-center px-5 sm:px-[10%] md:px-[15%]">
-        <ul className="flex w-full flex-col justify-center gap-5">
-          {latestTenPosts.map((metaData, index) => (
-            <BlogListItem key={index} {...metaData} />
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="mt-5 w-full justify-center px-5 sm:px-[10%] md:px-[15%]">
+      <ul className="flex w-full flex-col justify-center gap-5">
+        {latestTenPosts.map((metaData, index) => (
+          <BlogListItem key={index} {...metaData} />
+        ))}
+      </ul>
+    </div>
   );
 }
