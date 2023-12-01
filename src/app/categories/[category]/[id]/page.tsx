f1a582c60768ace2913e05eec8b13e16354cae99
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-01 10:29:33 +0900

    Feat: EmptyBlogMessage

--- src/app/categories/[category]/[id]/page.tsx
+++ src/app/categories/[category]/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { getPostData } from "../../../../../lib/posts";
 import PostHeader from "@/components/devlog/PostHeader";
-import { MainContents, BackButton } from "@/components/common";
+import { MainContents } from "@/components/common";
+import EmptyBlogMessage from "@/components/devlog/EmptyBlogMessage";
+
+const emptyHTMLString = "<html><head></head><body></body></html>";
 
 interface PostProps {
   params: {
@@ -17,15 +20,18 @@ export default async function Post({ params }: PostProps) {
 
   return (
     <MainContents>
-      <div className="flex h-full w-[80%] flex-col content-center items-center gap-10 p-10">
+      <div className="flex h-full w-full flex-col content-center items-center gap-2 p-3 sm:gap-10 sm:p-10 2xl:w-[60%]">
         <div className="w-full">
-          <BackButton />
           <PostHeader title={title} date={date} tags={tags} />
         </div>
-        <div
-          className="blog"
-          dangerouslySetInnerHTML={{ __html: modifiedHtmlContent }}
-        />
+        {modifiedHtmlContent !== emptyHTMLString ? (
+          <div
+            className="blog"
+            dangerouslySetInnerHTML={{ __html: modifiedHtmlContent }}
+          />
+        ) : (
+          <EmptyBlogMessage />
+        )}
       </div>
     </MainContents>
   );
---
+++ src/components/devlog/EmptyBlogMessage.tsx
@@ -0,0 +1,7 @@
+export default function EmptyBlogMessage() {
+  return (
+    <div className="flex h-[500px] w-full items-center justify-center">
+      <p className="font-bold italic text-gray-200">N/A</p>
+    </div>
+  );
+}
