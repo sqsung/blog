81545058223c3837d35c9cd393f5eaa5845f16cd
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-01 10:30:04 +0900

    Fix: delete unneeded pages

--- src/app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ContactPage() {
-  return (
-    <div>
-      <p>This is going to be used as a contact page!</p>
-    </div>
-  );
-}
--- src/app/devlog/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DevlogListPage() {
-  return (
-    <div>
-      <p>This is going to be used as a devlog list page!</p>
-    </div>
-  );
-}
