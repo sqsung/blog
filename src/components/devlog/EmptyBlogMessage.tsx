4f53bca111f6e98e5d410d62bf427213f995b48c
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-04 10:24:05 +0900

    Fix: EmptyBlogMessage props

--- src/components/devlog/EmptyBlogMessage.tsx
+++ src/components/devlog/EmptyBlogMessage.tsx
@@ -1,7 +1,15 @@
-export default function EmptyBlogMessage() {
+interface EmptyBlogMessageProps {
+  message?: string;
+  icon?: React.ReactNode;
+}
+
+export default function EmptyBlogMessage({
+  message = "N/A",
+  icon,
+}: EmptyBlogMessageProps) {
   return (
     <div className="flex h-[500px] w-full items-center justify-center">
-      <p className="font-bold italic text-gray-200">N/A</p>
+      <p className="font-bold italic text-gray-200">{icon ? icon : message}</p>
     </div>
   );
 }
