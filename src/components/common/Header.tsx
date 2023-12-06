45d4ad9da58a6dff942a6a9b5dd31109b3cee717
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-06 19:26:16 +0900

    Style: changed image files
    - Changed favicon
    - Added logo image file and to Header
    - Changed Header styles to fit the logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="mb-auto flex h-[50px] items-center justify-between border-b-[1px] px-1 md:px-10">
-      <Link href="/">
-        <p className="cursor-pointer text-2xl font-bold italic text-blue-300">
-          <span className="not-italic">🐋</span> sqsung devlog
+      <Link href="/" className="flex items-center justify-center gap-3">
+        <Image
+          src="/images/logo.png"
+          alt="jsjs devlog logo"
+          width={30}
+          height={30}
+        />
+        <p className="cursor-pointer text-2xl font-bold italic text-[#5AF]">
+          jsjs devlog
         </p>
       </Link>
     </div>
