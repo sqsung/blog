afc5a926c40d3dced7909b40e1ef5d3c3aa670dc
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-13 11:46:46 +0900

    Style: tweaked styles to fit new color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "rgb(35, 47, 62)",
+        secondary: "rgb(45, 55, 67)",
+      },
     },
   },
   plugins: [],
