401d88c91ef7ae1745206c711f2e8ec77c06da48
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-14 10:43:09 +0900

    Fix: BlogListItem title overflow error

--- src/components/devlog/BlogListItem.tsx
+++ src/components/devlog/BlogListItem.tsx
@@ -8,7 +8,7 @@ export default function BlogListItem(props: PostData) {
   const { id, thumbnail, tags, title, description, date, category } = props;
 
   return (
-    <li className="title-text gray-border i-hover-up t-hover-blue h-80 overflow-hidden rounded-md border sm:h-fit sm:pb-0">
+    <li className="title-text gray-border i-hover-up t-hover-blue h-[22rem] overflow-hidden rounded-md border sm:h-fit sm:pb-0">
       <Link href={`/devlog/${category}/${id}`}>
         <div className="flex h-[100%] flex-col gap-2 sm:flex-row">
           <Image
@@ -19,14 +19,14 @@ export default function BlogListItem(props: PostData) {
             className="h-[60%] w-full min-w-[200px] object-cover sm:h-[200px] sm:max-h-[200px] sm:w-[200px]"
           />
           <div className="flex h-full w-full flex-col p-1 sm:p-2">
-            <p className="overflow-ellipsis whitespace-nowrap text-sm font-bold sm:text-lg">
+            <p className="line-clamp-2 overflow-ellipsis text-sm font-bold sm:text-lg">
               {title}
             </p>
             <p className="subtle-text text-xs sm:text-sm ">
               {`${formatDate(date)}`}
             </p>
             <Tags tags={tags} />
-            <p className="subtle-text my-1 line-clamp-2 overflow-hidden overflow-ellipsis py-1 text-xs sm:my-3 sm:text-sm">
+            <p className="subtle-text my-2 line-clamp-2 overflow-ellipsis text-xs sm:my-3 sm:text-sm">
               {description}
             </p>
           </div>
