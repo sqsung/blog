79956aa468482adc29f9b038d4724ec1686d4607
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-18 05:58:26 +0900

    Fix: blog page desktop width

--- src/app/devlog/[category]/[id]/loading.tsx
+++ src/app/devlog/[category]/[id]/loading.tsx
@@ -5,7 +5,7 @@ import { CircularProgress } from "@mui/material";
 export default function PostLoading() {
   return (
     <MainContents>
-      <div className="flex h-full w-full flex-col content-center items-center gap-2 p-3 sm:gap-10 sm:p-10 lg:w-[60%]">
+      <div className="flex h-full w-full flex-col content-center items-center gap-2 p-3 sm:gap-10 sm:p-10 lg:w-[50%]">
         <div className="w-full">
           <PostHeaderSkeleton />
         </div>
