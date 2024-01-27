ce30b281ab142c84014565ecaee548952a29e461
Kyusung Sohn <rok.ksohn@gmail.com>
2024-01-27 23:35:38 +0900

    Fix: change BlogListItem UI

--- src/components/devlog/BlogListItem.tsx
+++ src/components/devlog/BlogListItem.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import { formatDate } from "@/utils/format";
-import Tags from "./Tags";
 import { PostData } from "global";
 import { formatCategoryForUI } from "../../../lib/posts";
 
 export default function BlogListItem(props: PostData) {
-  const { id, thumbnail, tags, title, description, date, category } = props;
+  const { id, thumbnail, title, description, date, category } = props;
 
   return (
     <li className="title-text i-hover-up t-hover-blue overflow-hidden rounded-md">
       <Link href={`/devlog/${category}/${id}`}>
-        <div className="flex h-80 flex-col gap-2 sm:h-[26rem]">
-          <div className="relative h-[50%] min-h-[150px] overflow-hidden rounded-lg sm:max-h-[200px] sm:min-h-[200px]">
+        <div className="flex h-80 flex-col sm:h-[26rem]">
+          <div className="relative h-[50%] min-h-[150px] overflow-hidden rounded-t-lg sm:max-h-[200px] sm:min-h-[200px]">
             <Image
               src={thumbnail}
               alt="Blog Post Thumbnail"
@@ -21,7 +20,7 @@ export default function BlogListItem(props: PostData) {
               quality={75}
             />
           </div>
-          <div className="flex h-[50%] w-full flex-col p-1 sm:justify-between">
+          <div className="flex h-[50%] w-full flex-col justify-between border-b border-gray-700 p-0 py-2 shadow-md sm:rounded-b-lg sm:border-none sm:p-3">
             <div className="flex flex-col">
               <p className="subtle-text text-xs">
                 {formatCategoryForUI(category)}
@@ -29,14 +28,15 @@ export default function BlogListItem(props: PostData) {
               <p className="line-clamp-2 overflow-ellipsis text-base font-bold sm:text-lg">
                 {title}
               </p>
-              <Tags tags={tags} />
             </div>
             <p className="subtle-text my-2 line-clamp-2 overflow-ellipsis text-sm sm:my-3 sm:text-base">
               {description}
             </p>
-            <p className="subtle-text text-xs sm:text-sm ">
+
+            <div className="subtle-text flex gap-2 text-xs">
+              <i className="bi bi-calendar"></i>
               {`${formatDate(date)}`}
-            </p>
+            </div>
           </div>
         </div>
       </Link>
