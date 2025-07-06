a3babcf3462bcf641b94a3c4a99e7e36a7e2a1ab
Kyusung Sohn <rok.ksohn@gmail.com>
2025-07-06 12:21:17 +0900

    Common/ThemeToggler

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import ThemeToggler from "./ThemeToggler";
+
+export default function Header() {
+  return (
+    <header className="mb-10 flex w-full items-center py-5">
+      <Link href="/" className="mr-auto text-2xl font-bold">
+        {"<Sqsung />"}
+      </Link>
+      <div className="flex items-end gap-5 font-bold">
+        <Link href="/posts">posts</Link>
+        <Link href="/tags">tags</Link>
+        <Link href="/about">about</Link>
+        <ThemeToggler />
+      </div>
+    </header>
+  );
+}
