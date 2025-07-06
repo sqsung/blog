ba57d38ae36b27759dcfc555263870dc6027a91b
Kyusung Sohn <rok.ksohn@gmail.com>
2025-07-06 16:09:13 +0900

    Common/BackButton

---
+++ src/components/common/BackButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ArrowLongLeftIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
+
+const BackButton = () => {
+  const router = useRouter();
+
+  const onBack = () => {
+    const referrer = document.referrer;
+    const origin = window.location.origin;
+
+    if (!referrer || !referrer.startsWith(origin)) {
+      router.push("/");
+    } else {
+      router.back();
+    }
+  };
+
+  return (
+    <button className="group w-fit pe-5" onClick={onBack}>
+      <ArrowLongLeftIcon className="text-t-subtle transcolor group-hover:text-t-normal h-10 w-10 cursor-pointer" />
+    </button>
+  );
+};
+export default BackButton;
