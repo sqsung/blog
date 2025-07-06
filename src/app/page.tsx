1bf0df6b1a070350aae1f64865d4503d4b816842
Kyusung Sohn <rok.ksohn@gmail.com>
2025-07-06 12:21:38 +0900

    Common/ThemeProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import BlogList from "@/components/blog/BlogList";
-import Header from "@/components/layout/Header";
+import Header from "@/components/common/Header";
 
 const Home = () => {
   return (
