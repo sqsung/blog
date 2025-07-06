2af2639dea85743011f11f7389f7bb9e4a2e99c9
Kyusung Sohn <rok.ksohn@gmail.com>
2025-07-06 13:34:42 +0900

    Configure fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/lib/ThemeProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const sourceCodePro = Source_Code_Pro({
+  variable: "--font-source-code-pro",
   subsets: ["latin"],
 });
 
@@ -26,7 +26,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sourceCodePro.variable} ${inter.variable} antialiased`}
       >
         <ThemeProvider>{children}</ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,14 @@ import Header from "@/components/common/Header";
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex w-full max-w-[800px] flex-col gap-10">
+      <div className="flex w-full max-w-[1000px] flex-col gap-10">
         <Header />
 
-        <div>
-          <h2 className="text-5xl font-bold">Recent Posts</h2>
-          <p className="text-lg text-zinc-400">by sqsung</p>
-        </div>
-
         <BlogList
+          listAlias="Recent Posts"
           blogs={[
             {
+              id: "1",
               title: "How does DNS work under the hood?",
               summary:
                 "Core concepts you need to know to understand how your website is registered to the world wide web.",
@@ -22,7 +19,27 @@ const Home = () => {
               tags: ["Network", "DNS"],
               category: "Network",
               isPublished: true,
-              id: "1",
+            },
+            {
+              id: "2",
+              title:
+                "How does DNS work under the hood? For the length of the title. The CSS NextJS of the future",
+              summary:
+                "Core concepts you need to know to understand how your website is registered to the world wide web. The asdjkhjkfgh askhksafd askjdklasjd",
+              createdAt: "2025-07-05",
+              tags: ["Network", "DNS"],
+              category: "Network",
+              isPublished: true,
+            },
+            {
+              id: "3",
+              title: "How does DNS work under the hood?",
+              summary:
+                "Core concepts you need to know to understand how your website is registered to the world wide web.",
+              createdAt: "2025-07-04",
+              tags: ["Network", "DNS"],
+              category: "Network",
+              isPublished: true,
             },
           ]}
         />
