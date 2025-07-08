61ed0252ce7fdbc54dc66a19f9a61c694494ff2f
Kyusung Sohn <rok.ksohn@gmail.com>
2025-07-08 12:53:47 +0900

    Fix BlogCard cursor issue

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -14,14 +14,14 @@ const BlogCard = ({ metadata }: BlogCardProps) => {
 
   return (
     <li className="border-b-primary border-b first-of-type:border-y">
-      <div className="flex cursor-pointer flex-row gap-5 py-8">
+      <div className="flex flex-row gap-5 py-8">
         <p className="text-t-subtle me-10 mt-3 flex-shrink-0 tracking-wide">
           {metadata.createdAt}
         </p>
 
         <div className="flex w-full flex-col gap-5">
           <button
-            className="hover:bg-background-secondary transcolor flex flex-col gap-3 rounded-xl px-5 pt-3 pb-8"
+            className="hover:bg-background-secondary transcolor flex cursor-pointer flex-col gap-3 rounded-xl px-5 pt-3 pb-8"
             onClick={() => router.push(ROUTES.post(metadata.id))}
           >
             <p className="text-start text-2xl font-bold">{metadata.title}</p>
